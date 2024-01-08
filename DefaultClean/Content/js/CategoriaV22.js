d3e9bdfc740ac6bfd979b6442ae9419f71a3d6f1
Felipe Bebber <fbebber1@gmail.com>
2024-01-08 11:05:12 -0300

    desenv

--- DefaultClean/Content/js/CategoriaV22.js
+++ DefaultClean/Content/js/CategoriaV22.js
@@ -4,6 +4,25 @@ $(document).ready(function () {
         $('body').toggleClass('dg-isBlockedScroll');
     });
 
+    function findGetParameter(parameterName) {
+        var result = null,
+            tmp = [];
+        location.search
+            .substr(1)
+            .split("&")
+            .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+            });
+        return result;
+    }
+
+    var fv = findGetParameter('fv');
+
+    if (fv) {
+        $('.jsCategoriaSidebarLimparPreco').removeClass('dg-hide');
+    }
+
     var CarregarMais = {
         pagina: 1,
         tipo: null,
