5a0db564360ed64247fee3f2b5bf54b9dd2c9d95
Felipe Bebber <fbebber1@gmail.com>
2024-02-16 11:24:48 -0300

    remoção backup atual

--- 2/Content/js/Checkout.js
+++
@@ -1,8 +0,0 @@
-function callBackEditaEndereco(control) {
-    if (control) {
-        $('body').addClass('edit-endereco-page');
-    } else {
-        $('body').removeClass('edit-endereco-page');
-    }
-
-}
